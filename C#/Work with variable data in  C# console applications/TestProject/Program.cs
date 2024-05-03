1cb1ef4527b58348e796ae272d58a52b566328f1
Luciano Joaquín Campardón <argentinianlinuxnewbie@gmail.com>
2024-05-03 20:45:18 -0300

    COMPLETED THE SECOND MODULE OF THE C# PART 4  LEARNING PATH

--- C#/Work with variable data in  C# console applications/TestProject/Program.cs
+++ C#/Work with variable data in  C# console applications/TestProject/Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿string[] values = {"12.3", "45", "ABC", "11", "DEF"};
+decimal numericValue = 0;
+decimal accumulator = 0;
+string alphabeticValue = "";
+
+foreach (string value in values)
+{
+    if(decimal.TryParse(value, out numericValue))
+    {
+        accumulator += numericValue;
+    }
+    else
+    {
+        alphabeticValue += value;
+    }
+}
+
+Console.WriteLine($"\nMessage: {alphabeticValue}");
+Console.WriteLine($"Total: {accumulator}\n");
+
+
