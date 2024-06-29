125fcd5ef5495cf9732cb67af7d19f9df0bb8c69
Luciano Joaquín Campardón <argentinianlinuxnewbie@gmail.com>
2024-06-29 09:55:50 -0300

    FINISHED SECOND MODULE OF C# LEARNING PATH, PART 5

---
+++ C#/Create methods in C# console applications/Create C# methods with parameters/Exercise 1 - DISPLAY EMAIL ADDRESSES/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Security;
+
+string[,] corporate =
+{
+    {"Robert", "Bavin"}, {"Simon", "Bright"},
+    {"Kim", "Sinclair"}, {"Aashrita", "Kamath"},
+    {"Sarah", "Delucchi"}, {"Sinan", "Ali"}
+};
+
+string[,] external =
+{
+    {"Vinnie", "Ashton"}, {"Cody", "Dysart"},
+    {"Shay", "Lawrence"}, {"Daren", "Valdes"}
+};
+
+string internalDomain = "@contoso.com";
+
+DisplayEmailAddresses(corporate, external);
+
+void DisplayEmailAddresses(string[,] corpoArray, string[,] extArray, string domain = "@hayworth.com")
+{
+    string partialEmail = "";
+
+    for (int i = 0; i < corporate.GetLength(0); i++)
+    {
+        if (corporate[i, 0].Equals(corpoArray[i, 0]))
+        {
+            partialEmail = ((corpoArray[i, 0].ToString()).Substring(0, 2) + corpoArray[i, 1]).ToLower();
+            Console.WriteLine($"{partialEmail + internalDomain}");
+        }
+    }
+
+    for (int i = 0; i < external.GetLength(0); i++)
+    {
+        if (external[i, 0].Equals(extArray[i,0]))
+        {
+            partialEmail = ((extArray[i, 0].ToString()).Substring(0,2) + extArray[i, 1]).ToLower();
+            Console.WriteLine($"{partialEmail + domain}");
+
+        }
+    }
+}
+
---
+++ C#/Create methods in C# console applications/Write your first C# method/TestProject1/Program.cs
@@ -0,0 +1,89 @@
+﻿/*
+if ipAddress consists of 4 numbers
+and
+if each ipAddress number has no leading zeroes
+and
+if each ipAddress number is in range 0 - 255
+
+then ipAddress is valid
+
+else ipAddress is invalid
+*/
+
+string[] ipArray = new string[5];
+string[] address;
+bool validLength = false;
+bool validZeroes = false;
+bool validRange = false;
+
+for (int i = 0; i < ipArray.Length; i++)
+{
+    string randomIp = CreateIPv4Adress();
+    ipArray[i] = randomIp;
+}
+
+foreach (string ip in ipArray)
+{
+    address = ip.Split(".", StringSplitOptions.RemoveEmptyEntries);
+
+    ValidateLength();
+    ValidateZeroes();
+    ValidateRange();
+
+    if (validLength && validZeroes && validRange)
+    {
+        Console.WriteLine($"The ip {ip} is a valid IPv4 address");
+    }
+    else
+    {
+        Console.WriteLine($"The ip {ip} is an invalid IPv4 address");
+    }
+}
+
+void ValidateLength()
+{
+    validLength = address.Length == 4;
+}
+
+void ValidateZeroes()
+{
+    foreach (string number in address)
+    {
+        if (number.Length > 1 && number.StartsWith("0"))
+        {
+            validZeroes = false;
+            return;
+        }
+    }
+
+    validZeroes = true;
+}
+
+void ValidateRange()
+{
+    foreach (string number in address)
+    {
+        int value = int.Parse(number);
+        if (value < 0 || value > 255)
+        {
+            validRange = false;
+            return;
+        }
+    }
+
+    validRange = true;
+}
+
+string CreateIPv4Adress()
+{
+    Random random = new Random();
+    int[] ipv4parts = new int[4];
+
+    for (int i = 0; i < ipv4parts.Length; i++)
+    {
+        ipv4parts[i] = random.Next(0, 256);
+    }
+
+    string ipv4ip = string.Join(".", ipv4parts);
+    return ipv4ip;
+};
